56704866dfacd3bad11418ac4253552de8522176
Hemant Gupta <live.hemant@gmail.com>
2018-10-25 22:07:25 +0530

    1st commit

---
+++ app/controllers/home.js
@@ -0,0 +1,48 @@
+var numeral = require('numeral');
+var bcrypt = require('bcrypt-nodejs');
+var dateFormat = require('dateformat');
+var constants = require('../../config/constants');
+
+exports.loggedIn = function(req, res, next)
+{
+	if (req.session.user) { // req.session.passport._id
+		next();
+	} else {
+		res.redirect('/login');
+	}
+}
+
+exports.home = function(req, res) {
+	res.render('home.ejs', {
+		constants : constants,
+		error : req.flash("error"),
+		success: req.flash("success"),
+		session:req.session,
+	 });
+}
+
+exports.signup = function(req, res) {
+	if (req.session.user) {
+		res.redirect('/home');
+	} else {
+		res.render('signup', {
+			constants : constants,
+			error : req.flash("error"),
+			success: req.flash("success"),
+			session:req.session
+		});
+	}
+}
+
+exports.login = function(req, res) {	
+	if (req.session.user) {
+		res.redirect('/home');
+	} else {
+		res.render('login', {
+			constants : constants,
+			error : req.flash("error"),
+			success: req.flash("success"),
+			session:req.session
+		});
+	}
+}
---
+++ config/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'project_name':'Analytics',
+	'adminname': 'hemant',
+	'adminmail': 'test@email.com',
+	'host':"http://localhost:8042",
+	'email_smtp_host':'Your Email SMTP',
+	'email_smtp_port':'YOUR SMTP PORT',
+	"smtp_from_eamil":"from@email.com",
+	"smtp_from_name":"website name",
+	"alert_email":"alert@email.com",
+	"alert_email_name":"Alert Name",
+};
