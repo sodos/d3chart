39b48db6c67ba6b390ce781fcad19afa42faa6f7
Hemant Gupta <live.hemant@gmail.com>
2018-10-29 19:53:59 +0530

    bar chart added - with sale

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -17,11 +17,18 @@ exports.home = function(req, res) {
 	var return_array_zone = [];
 	var return_array_state = [];
 	var return_array_city = [];
+    var return_array_date = [];
+    var return_array_date_s = [];
+    var return_array_date_z = [];
     
     var check_array_zone = [];
     var check_array_state = [];
     var check_array_city = [];
-	record.find().select(['state','city']).exec(function(err, records) {
+    var check_array_date = [];
+    var check_array_date_s = [];
+    var check_array_date_z = [];
+
+	record.find().select(['state','city','bill_date','net_amount_sum']).exec(function(err, records) {
 
 		// console.log(records);
 		// var north_zone = ['Jammu and Kashmir','Himachal Pradesh','Punjab','Uttarakhand','Uttar Pradesh','Haryana'];
@@ -33,8 +40,8 @@ exports.home = function(req, res) {
 		var zones = {'new delhi':'North Zone','jammu and kashmir':'North Zone','himachal pradesh':'North Zone','punjab':'North Zone','uttarakhand':'North Zone','uttar pradesh':'North Zone','haryana':'North Zone','bihar':'East Zone','orissa':'East Zone','jharkhand':'East Zone','west bengal':'East Zone','rajasthan':'West Zone','gujrat':'West Zone','goa':'West Zone','maharashtra':'West Zone','andhra pradesh':'South Zone','karnataka':'South Zone','kerala':'South Zone','tamil nadu':'South Zone','madhya pradesh':'Central Zone','chhattisgarh':'Central Zone','assam':'North East Zone','sikkim':'North East Zone','nagaland':'North East Zone','meghalaya':'North East Zone','manipur':'North East Zone','mizoram':'North East Zone','tripura':'North East Zone','arunachal pradesh':'North East Zone'};
 		//@ build the final return_array_state for retuning
         records.forEach(function(row){
-            
-            var zone = zones[row.state.toLowerCase()]
+                        
+            var zone = zones[row.state.toLowerCase()];
             var index_zone = check_array_zone.indexOf(zone);           	
             if(check_array_zone.indexOf(zone) < 0){
                 check_array_zone.push(zone);
@@ -62,8 +69,46 @@ exports.home = function(req, res) {
                 return_array_city[index_city].sale = return_array_city[index_city].sale + 1;
             } 
 
+            var d = new Date(row.bill_date);
+            var c = d.getMonth() + '_' + d.getFullYear();            
+            var index_date = check_array_date.indexOf(c);
+            if(check_array_date.indexOf(c) < 0){
+                check_array_date.push(c);
+                tmp = d.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+                return_array_date.push({date:tmp,sale:1, net_amount_sum:row.net_amount_sum});
+            }else{
+                //@ update the sale count
+                return_array_date[index_date].sale = return_array_date[index_date].sale + 1;
+                return_array_date[index_date].net_amount_sum = return_array_date[index_date].net_amount_sum + row.net_amount_sum;
+            }
+                        
+            var c_s = d.getMonth() + '_' + d.getFullYear() + '_' + row.state.toLowerCase();
+            var index_date_s = check_array_date_s.indexOf(c_s);
+            if(check_array_date_s.indexOf(c_s) < 0){
+                check_array_date_s.push(c_s);
+                tmp = d.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+                return_array_date_s.push({state:row.state,date:tmp,sale:1, net_amount_sum:row.net_amount_sum});
+            }else{
+                //@ update the sale count
+                return_array_date_s[index_date_s].sale = return_array_date_s[index_date_s].sale + 1;
+                return_array_date_s[index_date].net_amount_sum = return_array_date_s[index_date].net_amount_sum + row.net_amount_sum;
+            }
+
+            var c_z = d.getMonth() + '_' + d.getFullYear() + '_' + zone;
+            var index_date_z = check_array_date_z.indexOf(c_z);
+            if(check_array_date_z.indexOf(c_z) < 0){
+                check_array_date_z.push(c_z);
+                tmp = d.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+                return_array_date_z.push({zone:zone,date:tmp,sale:1, net_amount_sum:row.net_amount_sum});
+            }else{
+                //@ update the sale count
+                return_array_date_z[index_date_z].sale = return_array_date_z[index_date_z].sale + 1;
+                return_array_date_z[index_date].net_amount_sum = return_array_date_z[index_date].net_amount_sum + row.net_amount_sum;
+            }
+
         });
         
+
         // //@ sorting array to final        
         return_array_zone.sort(function(a, b){
             var nameA=a.zone.toLowerCase(), nameB=b.zone.toLowerCase()
@@ -90,10 +135,40 @@ exports.home = function(req, res) {
             return 0 //default return value (no sorting)
         });
 
+        return_array_date.sort(function(a, b){
+            var nameA=a.date.toLowerCase(), nameB=b.date.toLowerCase()
+            if (nameA < nameB) //sort string ascending
+                return -1 
+            if (nameA > nameB)
+                return 1
+            return 0 //default return value (no sorting)
+        });
+
+        return_array_date_z.sort(function(a, b){
+            var nameA=a.date.toLowerCase(), nameB=b.date.toLowerCase()
+            if (nameA < nameB) //sort string ascending
+                return -1 
+            if (nameA > nameB)
+                return 1
+            return 0 //default return value (no sorting)
+        });
+
+        return_array_date_s.sort(function(a, b){
+            var nameA=a.date.toLowerCase(), nameB=b.date.toLowerCase()
+            if (nameA < nameB) //sort string ascending
+                return -1 
+            if (nameA > nameB)
+                return 1
+            return 0 //default return value (no sorting)
+        });
+
 		res.render('home.ejs', {
 			sales_zone:return_array_zone,
 			sales_state:return_array_state,
 			sales_city:return_array_city,
+			sales_date:return_array_date,
+			sales_date_s:return_array_date_s,
+			sales_date_z:return_array_date_z,
 			constants : constants,
 			error : req.flash("error"),
 			success: req.flash("success"),
